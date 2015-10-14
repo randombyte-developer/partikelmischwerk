902131e57c1cd2d5b3bb2d1578f6612fa4f6da42
RandomByte <apps.randombyte@gmail.com>
2015-10-14 18:30:38 +0200

    Added parsing of SimpleDemo.json

--- partikelmischwerk.rb
+++ partikelmischwerk.rb
@@ -1,48 +1,45 @@
 require 'gosu'
+require 'json'
 
 class ParticleWindow < Gosu::Window
 
-	DEAFULT_GRAVITY = 0.05
+	$DEFAULT_GRAVITY = 0.05
 
 	def initialize(width = 800, height = 600, fullscreen = true)
 		super
 		self.caption = "Particles"
 
-		@width = width
-		@height = height
-		@intensity = 1
-		@font = Gosu::Font.new(20)
+		$width = width
+		$height = height
+		@font = Gosu::Font.new(10)
 		@particles = []
-		@r = Random.new
+		$r = Random.new
+		@controller = Parser.new.parse_file("./SimpleDemo.json")
+		# puts @controller
+		@frame = 1
 	end
 
 	def button_down(id)
 		close if id == Gosu::KbEscape
 		@particles.clear if id == Gosu::KbC
+		@frame = 0 if id == Gosu::KbS
 	end
 
 	def update
-		@intensity += 1 if Gosu.button_down?(Gosu::KbUp)
-		@intensity -= 1 if Gosu.button_down?(Gosu::KbDown) && @intensity > 1
-		if Gosu.button_down?(Gosu::MsLeft)
-			particle_spawner(mouse_x, mouse_y, -8..8, 3..4, random_color(), @intensity)
-		end
+		# @intensity += 1 if Gosu.button_down?(Gosu::KbUp)
+		# @intensity -= 1 if Gosu.button_down?(Gosu::KbDown) && @intensity > 1
+		# if Gosu.button_down?(Gosu::MsLeft)
+		# 	particle_spawner(mouse_x, mouse_y, -8..8, 3..4, random_color(), @intensity)
+		# end
+		# @particles.each(&:update)
+		@controller.update(@particles, @frame)
 		@particles.each(&:update)
-		@particles.delete_if { |particle| !particle.x.between?(0, @width) || !particle.y.between?(-@height, @height) }
-	end
-
-	def particle_spawner(x, y, xd_range, yd_range, c, particles_count)
-		particles_count.times do
-			@particles.push(Particle.new(x, y, @r.rand(xd_range), -@r.rand(yd_range), c, DEAFULT_GRAVITY))
-		end
-	end
-
-	def random_color
-		Gosu::Color.new(@r.rand(0..255), @r.rand(0..255), @r.rand(0..255))
+		@frame += 1
+		@particles.delete_if { |particle| !particle.x.between?(0, $width) || !particle.y.between?(-$height, $height) }
 	end
 
 	def draw
-		@font.draw("Particles: #{@particles.size}", 10, 10, 0)
+		@font.draw("Particles: #{@particles.size}, Frame: #{@frame}, Fps: #{Gosu.fps}", 10, $height - 10, 0)
 		Gosu.draw_rect(mouse_x, mouse_y, 10, 10, Gosu::Color.new(255, 255, 255, 0))
 		@particles.each(&:draw)
 	end
@@ -69,6 +66,61 @@ class Particle
 	def draw
 		Gosu.draw_rect(@x, @y, 10, 10, @c)
 	end
+end
+
+class Parser
+	def parse_file(filename)
+		parse(File.read(filename))
+	end
+
+	def parse(input)
+		parsed = JSON.parse(input)
+		items = []
+		parsed["timeline"].each do |timeline_item|
+			item_data = {}
+			timeline_item.each do |k, v|
+		 		if v.is_a? String
+					v = v.gsub("h", $height.to_s).gsub("w", $width.to_s)
+		  		if (k == "x" || k == "y" || k == "xd" || k == "yd" || k == "r" || k == "g" || k == "b") && v.include?("~")
+						item_data[k] = parse_range(v)
+					else
+						item_data[k] = v.to_i
+					end
+				else
+					item_data[k] = v
+				end
+			end
+			items.push item_data
+		end
+		Controller.new(items, parsed["duration"])
+	end
+
+	def parse_range(input)
+		splits = input.split("~")
+		splits.first.to_f..splits.last.to_f
+	end
+end
+
+class Controller
+		def initialize(data, duration)
+			@data = data
+			@duration = duration
+		end
+
+		def update(particles, frame)
+			@data.each do |item|
+				next if !(item["at"]..(item["at"] + item["d"] - 1)).include?(frame)
+				item["n"].times { particles.push(Particle.new(get_value(item["x"]), get_value(item["y"]), get_value(item["xd"]), get_value(item["yd"]), Gosu::Color.new(get_value(item["r"]), get_value(item["g"]), get_value(item["b"])), $DEFAULT_GRAVITY)) }
+			end
+		end
+
+		def get_value(property)
+			property.is_a?(Range) ? $r.rand(property) : property
+		end
+
+		def to_s
+			"Data: #{@data}"
+		end
 end
 
 ParticleWindow.new.show()
